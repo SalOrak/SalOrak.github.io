d55864f3f8eaecb904c63cad4156dc9732dc270b
SalOrak <salorack@protonmail.com>
2022-07-25 00:40:31 +0200

    Version 0.0 Fixed Message via Script

--- _nuxt/static/1658702348/writeups/grandpa-htb/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658702348",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwriteups\u002Fgrandpa-htb",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"11316e1f"}}}}(null));
