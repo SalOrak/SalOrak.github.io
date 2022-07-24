95aed1cc1d1cac6002c940ca0518f8aa0e599b6b
SalOrak <salorack@protonmail.com>
2022-07-25 00:37:55 +0200

    Version 0.0 Fixed Message via Script

--- _nuxt/static/1658702143/writeups/devel-htb/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsalorak.github.io\u002F_nuxt\u002Fstatic\u002F1658702143",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwriteups\u002Fdevel-htb",config:{_app:{basePath:"\u002Fsalorak.github.io\u002F",assetsPath:"\u002Fsalorak.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"51ec631d"}}}}(null));
