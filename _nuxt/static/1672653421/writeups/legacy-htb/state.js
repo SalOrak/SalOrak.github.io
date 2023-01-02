f920a8e74d1d2e5fb0bcdf6df72ee39a547b7b7d
SalOrak <salorack@protonmail.com>
2023-01-02 11:56:20 +0100

    Version 0.0 Fixed Message via Script

--- _nuxt/static/1672653421/writeups/legacy-htb/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672653421",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwriteups\u002Flegacy-htb",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f3d43f0f"}}}}(null));
